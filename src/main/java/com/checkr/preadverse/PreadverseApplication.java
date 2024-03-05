08cbaa0d8ad456ebf25f526fac85cb7d72124dc6
Raj-LDP <raj.yadav@zemosolabs.com>
2024-03-05 08:27:55 +0530

    setup repository

---
+++ src/main/java/com/checkr/preadverse/PreadverseApplication.java
@@ -0,0 +1,13 @@
+package com.checkr.preadverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreadverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreadverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkr/preadverse/PreadverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkr.preadverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreadverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
